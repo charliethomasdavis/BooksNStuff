27e85bcf60736b048a25a202655bd8c75af8d1b4
charliethomasdavis <charles.tb.davis@gmail.com>
2019-11-26 20:47:04 -0600

    Add search and filter on book api

--- backendapi/api/views.py
+++ backendapi/api/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import UserSerializer, BooksSerializer, AuthorsSerializer
 from api.models import Books, Authors
 
@@ -10,14 +12,20 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class BooksViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
-    authentication_classes = [TokenAuthentication, ]
-    permission_classes = [IsAuthenticated, ]
+    #authentication_classes = [TokenAuthentication, ]
+    #permission_classes = [IsAuthenticated, ]
+
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['isbn', 'title', 'price', 'pubdate', 'userreviews', 'sname']
+    search_fields = ['isbn', 'title']
+
 
 class AuthorsViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Authors.objects.all()
     serializer_class = AuthorsSerializer
-    authentication_classes = [TokenAuthentication, ]
-    permission_classes = [IsAuthenticated, ]
+    #authentication_classes = [TokenAuthentication, ]
+    #permission_classes = [IsAuthenticated, ]
